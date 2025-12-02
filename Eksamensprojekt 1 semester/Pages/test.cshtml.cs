579da27f1514e065a9a3ab8f8b8ebd028d90c269
Christian Allerslev Hansen <chh015@edu.zealand.dk>
2025-12-02 11:10:15 +0100

    Added a test

---
+++ Eksamensprojekt 1 semester/Pages/test.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Eksamensprojekt_1_semester.Pages
+{
+    public class testModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

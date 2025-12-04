32a6e9753fdc944fe038399f54b1a6f67568de99
Christian Allerslev Hansen <chh015@edu.zealand.dk>
2025-12-04 13:27:12 +0100

    Created a "bookedBoats" cshtml

---
+++ Eksamensprojekt 1 semester/Pages/Booking/BookedBoats.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Eksamensprojekt_1_semester.Pages.Booking
+{
+    public class BookedBoatsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

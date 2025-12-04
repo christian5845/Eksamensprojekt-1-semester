a50167b784defb754fceee3434d7566a81f041fd
Jonas Boye Jensen <joj008@edu.zealand.dk>
2025-12-04 11:18:22 +0100

    Added Booking class

---
+++ Eksamensprojekt 1 semester/Models/Booking.cs
@@ -0,0 +1,18 @@
+﻿namespace Eksamensprojekt_1_semester.Models
+{
+    public class Booking
+    {
+        public int BookingId { get; set; }
+        public string Name { get; set; }
+        public string Date { get; set; }
+        public double Price { get; set; }
+
+        public Booking() { }
+
+        public Booking(string name, string date)
+        {
+            Name = name;
+            Date = date;
+        }
+    }
+}

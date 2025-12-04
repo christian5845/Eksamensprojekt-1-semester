e756b231cd150e409c5305cbbf37521192faf014
Søren Palle Dyrbye <sod004@edu.zealand.dk>
2025-12-04 10:50:40 +0100

    Boat class

---
+++ Eksamensprojekt 1 semester/Models/Boats.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+namespace Eksamensprojekt_1_semester.Models
+{
+    public class Boats 
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public string Size { get; set; }
+
+        public Boats() // NB: Nødvendig fordi vi er i en Razor Pages app.
+        {
+            ErAktiv = true;
+        }
+    
+    public Boats(int id, string type, string size)
+        {
+            Id = id;
+            Type = type;
+            Size = size;
+        }
+    }
+}

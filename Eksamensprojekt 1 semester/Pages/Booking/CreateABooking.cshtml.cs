a8239c16f900f570cd91838d7d3ee87a8b111e37
Christian Allerslev Hansen <chh015@edu.zealand.dk>
2025-12-05 12:55:52 +0100

    Starting to add booking system with Rasmus and Søren - dosnt work yet

--- Eksamensprojekt 1 semester/Pages/Booking/CreateABooking.cshtml.cs
+++ Eksamensprojekt 1 semester/Pages/Booking/CreateABooking.cshtml.cs
@@ -2,12 +2,35 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Eksamensprojekt_1_semester.Models;
 using Eksamensprojekt_1_semester.MockData;
-namespace Eksamensprojekt_1_semester.Pages.Booking
+using Eksamensprojekt_1_semester.Services.Repositories;
+using Eksamensprojekt_1_semester.Services.Interfaces;
+namespace Eksamensprojekt_1_semester.Pages.Booking;
+
+public class CreateABookingModel : PageModel
 {
-    public class CreateABookingModel : PageModel
+    private IBookABoatRepository _iBookABoatRepository;
+    
+    [BindProperty]
+    public Booking Booking;
+
+    public void OnGet()
     {
-        public void OnGet()
+
+    }
+
+    public CreateABookingModel(IBookABoatRepository iBookABoatRepository)
+    {
+        _iBookABoatRepository = iBookABoatRepository;
+    }
+
+    public IActionResult OnPost()
+    {
+        if (!ModelState.IsValid)
         {
+            return Page();
         }
+
+        _iBookABoatRepository.AddABoat(boat);
+        return RedirectToPage("GetAllMembers");
     }
 }

eabdd25a3c536d92a6f7627ffbeb28325c216ddf
Christian Allerslev Hansen <chh015@edu.zealand.dk>
2025-12-05 10:16:43 +0100

    Added BookABoat

---
+++ Eksamensprojekt 1 semester/Pages/Booking/BookABoat.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Eksamensprojekt_1_semester.Pages.Booking
+{
+    public class BookABoatModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

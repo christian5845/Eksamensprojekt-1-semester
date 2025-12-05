4ff3690c4cfed702b5c35d5c18226f3ebc06ac10
Jonas Boye Jensen <joj008@edu.zealand.dk>
2025-12-05 10:39:27 +0100

    Added propertien PicePerDay til Boat og MockBoats

--- Eksamensprojekt 1 semester/MockData/MockBoats.cs
+++ Eksamensprojekt 1 semester/MockData/MockBoats.cs
@@ -6,11 +6,11 @@ namespace Eksamensprojekt_1_semester.MockData
     {
         private static List<Boat> _boats = new List<Boat>
         {
-            new Boat(1, "TEVA", "Stor"),
-            new Boat(2, "WAVE", "Lille"),
-            new Boat(3, "SURFER", "Mellem"),
-            new Boat(4, "SPEEDY", "Stor"),
-            new Boat(5, "FLOATY", "Lille"),
+            new Boat(1, "TEVA", "Stor", 1000.0),
+            new Boat(2, "WAVE", "Lille", 500.0),
+            new Boat(3, "SURFER", "Mellem", 750.0),
+            new Boat(4, "SPEEDY", "Stor", 1200.0),
+            new Boat(5, "FLOATY", "Lille", 600.0),
         };
         public static List<Boat> GetMockBoats() { return _boats; }
     }
--- Eksamensprojekt 1 semester/Models/Boat.cs
+++ Eksamensprojekt 1 semester/Models/Boat.cs
@@ -7,13 +7,15 @@ public class Boat
     public int Id { get; set; }
     public string Type { get; set; }
     public string Size { get; set; }
+    public double PricePerDay { get; set; }
 
     public Boat() { }
 
-public Boat(int id, string type, string size)
+public Boat(int id, string type, string size, double pricePerDay)
     {
         Id = id;
         Type = type;
         Size = size;
+        PricePerDay = pricePerDay;
     }
 }

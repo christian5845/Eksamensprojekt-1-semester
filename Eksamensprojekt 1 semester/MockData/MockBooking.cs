421b867b615cda07905f8d63bb54eb03a9ada8f5
Jonas Boye Jensen <joj008@edu.zealand.dk>
2025-12-05 10:41:39 +0100

    Fixed conflict med MockBooking

--- Eksamensprojekt 1 semester/MockData/MockBooking.cs
+++ Eksamensprojekt 1 semester/MockData/MockBooking.cs
@@ -6,9 +6,9 @@ namespace Eksamensprojekt_1_semester.MockData
     {
         private static List<Booking> _booking = new List<Booking>
         {
-            new Booking("Jesper", "3. januar", new Boat(1, "TEVA", "Stor")),
-            new Booking("Bo", "19. december",new Boat()),
-            new Booking("kalle", "27. december",new Boat())
+            new Booking("Jesper", "3. januar", new Boat(1, "TEVA", "Stor", 1000.0)),
+            new Booking("Bo", "19. december", new Boat(2, "WAVE", "Lille", 500.0)),
+            new Booking("kalle", "27. december", new Boat(3, "SURFER", "Mellem", 750.0))
         };
         public static List<Booking> GetMockBooking() { return _booking; }
 

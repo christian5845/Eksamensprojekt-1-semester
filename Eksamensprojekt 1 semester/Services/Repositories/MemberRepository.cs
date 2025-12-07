3a2dea84339099ce35555796b5792e164a8c19cf
Troels Aagaard Skalkam <troels@skalkam.dk>
2025-12-08 00:22:29 +0100

    lavet wwwroot/Data mappen, Json mappen, lavet Members.Json, jsonfilememberservice, og ændret i memberrepository, nu gemmes alle ændringer når man adder, editer og deleter members

--- Eksamensprojekt 1 semester/Services/Repositories/MemberRepository.cs
+++ Eksamensprojekt 1 semester/Services/Repositories/MemberRepository.cs
@@ -1,79 +1,100 @@
 ﻿using Eksamensprojekt_1_semester.Models;
-using Eksamensprojekt_1_semester.MockData;
 using Eksamensprojekt_1_semester.Services.Interfaces;
+using Eksamensprojekt_1_semester.Services.Json;
 
 namespace Eksamensprojekt_1_semester.Services.Repositories
 {
-    public class MemberRepository : IMemberRepository
+  public class MemberRepository : IMemberRepository
+  {
+    private List<Member> _members;
+
+    private JsonFileMemberService _jsonFileMemberService;
+
+    public MemberRepository(JsonFileMemberService jsonFileMemberService)
     {
-        private List<Member> _members;
-        public MemberRepository() 
-        {
-            _members = MockBookings.GetMockMembers();
-        }
+      _jsonFileMemberService = jsonFileMemberService;
 
-        public List<Member> GetMembers() 
-        { 
-        return _members;
-        }
-
-        public void AddMember(Member member) 
-        { 
-        _members.Add(member);
-        }
-
-
-        public IEnumerable<Member> NameSearch(string str)
-        {
-            List<Member> nameSearch = new List<Member>();
-            foreach (Member member in _members)
-            {
-                if (string.IsNullOrEmpty(str) || member.Name.ToLower().Contains(str.ToLower()))
-                {
-                    nameSearch.Add(member);
-                }
-            }
-
-            return nameSearch;
-        }
-        public void UpdateMember(Member member)
-        {
-            if (member != null)
-            {
-                foreach (Member m in _members)
-                {
-                    if (m.Id == member.Id)
-                    {
-                        m.Name = member.Name;
-                        m.Age = member.Age;
-                        m.PhoneNumber = member.PhoneNumber;
-                        m.Email = member.Email;
-                    }
-                }
-            }
-        }
-        public Member GetMember(int id)
-        {
-            foreach (var m in _members)
-            {
-                if (m.Id == id)
-                    return m;
-            }
-            return null; 
-        }
-        public Member DeleteMember(int id)
-        {
-            foreach(Member member in _members)
-                { 
-                if 
-                    (member.Id == id) 
-                { 
-                    _members.Remove(member);
-                    return member;
-                } 
-            }
-            return null;
-        }
-
+      _members = _jsonFileMemberService
+                  .GetJsonMembers()
+                  .ToList();
     }
+
+    public List<Member> GetMembers()
+    {
+      return _members;
+    }
+
+    public void AddMember(Member member)
+    {
+      _members.Add(member);
+      _jsonFileMemberService.SaveJsonMembers(_members);
+    }
+
+    public IEnumerable<Member> NameSearch(string str)
+    {
+      List<Member> nameSearch = new List<Member>();
+
+      foreach (Member member in _members)
+      {
+        if (string.IsNullOrEmpty(str) ||
+            member.Name.ToLower().Contains(str.ToLower()))
+        {
+          nameSearch.Add(member);
+        }
+      }
+
+      return nameSearch;
+    }
+
+    public void UpdateMember(Member member)
+    {
+      if (member != null)
+      {
+        foreach (Member m in _members)
+        {
+          if (m.Id == member.Id)
+          {
+            m.Name = member.Name;
+            m.Age = member.Age;
+            m.PhoneNumber = member.PhoneNumber;
+            m.Email = member.Email;
+          }
+        }
+
+        _jsonFileMemberService.SaveJsonMembers(_members);
+      }
+    }
+
+    public Member GetMember(int id)
+    {
+      foreach (var m in _members)
+      {
+        if (m.Id == id)
+          return m;
+      }
+      return null;
+    }
+
+    public Member DeleteMember(int id)
+    {
+      Member memberToDelete = null;
+
+      foreach (Member member in _members)
+      {
+        if (member.Id == id)
+        {
+          memberToDelete = member;
+          break;
+        }
+      }
+
+      if (memberToDelete != null)
+      {
+        _members.Remove(memberToDelete);
+        _jsonFileMemberService.SaveJsonMembers(_members);
+      }
+
+      return memberToDelete;
+    }
+  }
 }

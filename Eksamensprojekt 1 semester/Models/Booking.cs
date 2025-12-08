d2ef4fa1bf55d31793823cda42627700bfe6d301
Christian Allerslev Hansen <chh015@edu.zealand.dk>
2025-12-08 11:06:53 +0100

    Edited date to "Date start" Added "Date end" implemented date end to CreateABooking Fixed names from English to Danish

--- Eksamensprojekt 1 semester/Models/Booking.cs
+++ Eksamensprojekt 1 semester/Models/Booking.cs
@@ -4,17 +4,19 @@
     {
         public int BookingId { get; set; }
         public string Name { get; set; }
-        public DateOnly Date { get; set; }
+        public DateOnly DateStart { get; set; }
+        public DateOnly DateEnd { get; set; }
         public double Price { get; set; }
         public Boat BookedBoat { get; set; } // Changed from get-only to get; set;
 
         public Booking() { }
 
-        public Booking(string name, DateOnly date, Boat bookedboat)
+        public Booking(string name, DateOnly datestart, DateOnly dateend, Boat bookedboat)
         {
             Name = name;
             BookedBoat = bookedboat;
-            Date = date;
+            DateStart = datestart;
+            DateEnd = dateend;
         }
        
     }

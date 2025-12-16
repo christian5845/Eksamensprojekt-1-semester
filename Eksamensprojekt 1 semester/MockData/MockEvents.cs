ad98f29ac29e1577576b49dce57b98ca51ea4747
Christian Allerslev Hansen <chh015@edu.zealand.dk>
2025-12-16 20:16:44 +0100

    Added Dateonly to events

--- Eksamensprojekt 1 semester/MockData/MockEvents.cs
+++ Eksamensprojekt 1 semester/MockData/MockEvents.cs
@@ -6,9 +6,9 @@ namespace Eksamensprojekt_1_semester.MockData
     {
         private static List<Event> _events = new List<Event>
         {
-            new Event(1, "Intro Kursus", "Kursus/Undervisning", 200.0),
-            new Event(2, "Grill og hygge på havnen", "Socialt arrangement", 50.0),
-            new Event(3, "Kapsejllads", "Konkurrence", 50.0)
+            new Event(1, "Intro Kursus", "Kursus/Undervisning", 200.0, new DateOnly (2025, 1, 2), new DateOnly (2025, 1, 2)),
+            new Event(2, "Grill og hygge på havnen", "Socialt arrangement", 50.0, new DateOnly(2025, 2, 2), new DateOnly(2025, 2, 2)),
+            new Event(3, "Kapsejllads", "Konkurrence", 50.0, new DateOnly(2025, 3, 2), new DateOnly(2025, 3, 2))
         };
         public static List<Event> GetMockEvents()
         {

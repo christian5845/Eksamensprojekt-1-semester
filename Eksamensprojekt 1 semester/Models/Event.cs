3d69884f331aa1201e3e624c1e7c4b5e1c7e0919
Søren Palle Dyrbye <sod004@edu.zealand.dk>
2025-12-17 09:22:55 +0100

    Småændringer

--- Eksamensprojekt 1 semester/Models/Event.cs
+++ Eksamensprojekt 1 semester/Models/Event.cs
@@ -7,13 +7,13 @@ public class Event
     public string Name { get; set; }
     public string Type { get; set; }
     public double Price { get; set; }
-    public DateOnly DateStart { get; set; }
-    public DateOnly DateEnd { get; set; }
+    public DateTime DateStart { get; set; }
+    public DateTime DateEnd { get; set; }
     #endregion
 
     #region constructors
     public Event() { }
-    public Event(int id, string name, string type, double price, DateOnly dateStart, DateOnly dateEnd)
+    public Event(int id, string name, string type, double price, DateTime dateStart, DateTime dateEnd)
     {
         Id = id;
         Name = name;
